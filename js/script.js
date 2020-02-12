00d0663d447a4fa2483ff72013d41bf694f2bb83
JiaLinYang0410 <jlyang5008@gmail.com>
2020-02-12 23:14:06 +0000

    20200212

---
+++ js/script.js
@@ -0,0 +1,24 @@
+songName = []
+songName.forEach(function(){
+
+}
+
+songArtist = []
+songArtist.forEach(function(){
+
+}
+
+songLength = []
+songLength.forEach(function(){
+
+}
+
+image = []
+image.forEach(function(){
+
+}
+
+link = []
+link.forEach(function(){
+
+}
